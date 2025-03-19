f99c813437c6fb037ea72a6380227fb516b382fa
eloa <francodosreissilvae@gmail.com>
2025-03-19 11:14:21 -0300

    refactor App component and add MovieCard component with modal functionality

---
+++ src/components/MovieDescription.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import MovieDescription from "../MovieDescription/MovieDescription";
+import styles from "./MovieCard.module.css";
+
+const MovieCard = (props) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  return (
+    <>
+      <div className={styles.movie} onClick={toggleModal}>
+        <div>
+          <p>{props.Year}</p>
+        </div>
+
+        <div>
+          <img src={props.Poster} alt="" />
+        </div>
+
+        <div>
+          <span>{props.Type}</span>
+          <h3>{props.Title}</h3>
+        </div>
+      </div>
+      {isModalOpen && (
+        <MovieDescription
+          apiUrl={props.apiUrl}
+          movieID={props.imdbID}
+          click={toggleModal}
+        />
+      )}
+    </>
+  );
+};
+
+export default MovieCard;
