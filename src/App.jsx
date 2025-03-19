3c7d5bb68aadd2e19105ef293435b5e830c9d563
eloa <francodosreissilvae@gmail.com>
2025-03-19 11:57:24 -0300

    projetozero

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import MovieCard from "./components/MovieCards/MovieCards";
+import MovieDescription from "./components/MovieDescription/MovieDescription.module.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   const [count, setCount] = useState()
