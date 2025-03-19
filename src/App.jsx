6d87c29f9ec1ceddddad0b95547ee7e107bfa82a
eloa <francodosreissilvae@gmail.com>
2025-03-19 15:02:37 -0300

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
 
   return (
     <>
+
       
       <h1></h1>
       <div className="card">
@@ -17,6 +18,7 @@ function App() {
         </button>
       </div>
       
+  <footer/>
     </>
   )
 }
