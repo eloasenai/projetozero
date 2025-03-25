5a540ccd74ffecd17368f267769fdbacecbdb8ee
eloa <francodosreissilvae@gmail.com>
2025-03-25 11:51:50 -0300

    add logo component and update background styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Logo from './Components/img/Captura de tela 2025-03-25 112327.png';
 
 export const App = () => {
   return (
     <div>
       <div className="top-bar"></div>
+      <Logo />
       {/* A imagem de fundo é definida no CSS */}
     </div>
   );
