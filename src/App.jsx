39e674923ffeaa545b99286c418f36e2f340ae37
Bianca <bianca.furlanetto440@gmail.com>
2025-03-25 14:56:48 -0300

    add search bar components and styles; update .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-//import imagem from "./components/img/fundo.png";
+
 import React from 'react';
 import './App.css';
 
@@ -9,10 +9,29 @@ export const App = () => {
         {/* <img src={imagem} alt='plano de fundo'/> */}
       </div> 
 
+
     <div>
       <div className="top-bar"></div>
       {/* A imagem de fundo é definida no CSS */}
     </div>
+
+    <div className="top-bar">
+          <input 
+            type="text" 
+            className="search-bar" 
+            placeholder="Pesquisar..." 
+
+          />
+           <input 
+            type="text" 
+            className="small-search-bar" 
+            placeholder="Categoria "
+            />
+             <div className="vertical-bars">
+          <div className="vertical-bar"></div>
+          <div className="vertical-bar"></div>
+        </div>
+        </div>
     </>
   );
 }
